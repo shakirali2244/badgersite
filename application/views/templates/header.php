9fc7a0bebb80268ee6831a0b295b4624a21430c1
Shakir Ali <shakirali2244@yahoo.com>
2016-02-29 03:27:17 -0500

    screencaps:

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -45,11 +45,11 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#"><?php echo $companyName; ?></a>
+              <a class="navbar-brand" href="#"><img src="/assets/img/logo.png" alt"<?php echo $companyName; ?>" height: 100%;/></a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
                 <li><?php echo anchor('/about', 'About');?></li>
                 <li><?php echo anchor('/contact', 'Contact');?></li>
                 <li class="dropdown">
