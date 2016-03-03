171672b8d995b58418c6be43f46211290de44e67
Shakir Ali <shakirali2244@yahoo.com>
2016-03-02 20:51:16 -0500

    test controller

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://shakirali.me';
+$config['base_url'] = 'http://shakirali.me/';
 
 /*
 |--------------------------------------------------------------------------
@@ -213,7 +213,7 @@ $config['directory_trigger'] = 'd';
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = 4;
 
 /*
 |--------------------------------------------------------------------------
@@ -238,7 +238,7 @@ $config['log_path'] = '';
 | Note: Leaving it blank will default to 'php'.
 |
 */
-$config['log_file_extension'] = '';
+$config['log_file_extension'] = 'log';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/routes.php
+++ application/config/routes.php
@@ -50,5 +50,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'pages/view';
-$route['(:any)'] = 'pages/view/$1';
+/*$route['(:any)'] = 'pages/view/$1';
+$route['blog/joe'] = "blogs/users/34";;*/
 
