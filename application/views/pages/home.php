4be35b78a4afdbcb253029fa672ea04468d4063c
root <root@ip-172-31-43-131.us-east-2.compute.internal>
2018-04-05 09:28:06 +0000

    updates with server config

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,21 +1,21 @@
- <div class="inner cover mid">
-            <h1 class="cover-heading"><p id="intro">Get out there.</p><br/> </h1>
-            <p class="lead"> Get your idea into dev stage. Check the <b><a href="/services">services</a></b> we provide.</p>
-            <!-- <p class="lead">
-              Single button 
+<div class="inner cover mid">
+            <h1 class="cover-heading"><p id="intro">Welcome to BadgerWorks</p><br/> </h1>
+            <p class="lead">Check some of my older work below or refer my Github  <b><a href="https://github.com/shakirali2244">here</a></b>.</p>
+            <p class="lead">
+            
               <div class="btn-group">
                 <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   Projects <span class="caret"></span>
                 </button>
                 <ul class="dropdown-menu">
-                  <li><a href="/basejumperpage">Basejumper</a></li>
+                  <li><a href="/game">2D physics Game</a></li>
                   <li role="separator" class="divider"></li>
-                  <li><a href="http://redditchat.us.to/">Redditchat</a></li>
-                  <li><a href="/mapchat/">Mapchat</a></li>
+                  <li><a href="http://redditchat.us.to/">Room based Websocket Chat</a></li>
                 </ul>
-
-            </p>
+                </p>
           </div>
-          <a href="/nyan"><h1>&#x1f431;</h1></a>-->
+          <!-- <a href="/nyan"><h1>&#x1f431;</h1></a>-->
           </p>
           </div>
+
+
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,6 @@
  <div class="mastfoot">
             <div class="inner">
-              <p>&copy; 2016 <?php echo $companyName; ?>, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+              <p>&copy; 2018 <?php echo $companyName; ?>, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
             </div>
           </div>
 
