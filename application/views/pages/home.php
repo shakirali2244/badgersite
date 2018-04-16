dcdc62baf378870be0b0ad5bfb3de9c9165bd708
root <root@ip-172-31-43-131.us-east-2.compute.internal>
2018-04-16 09:32:27 +0000

    adding avian link

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,23 +1,7 @@
 <div class="inner cover mid">
             <h1 class="cover-heading"><p id="intro">Welcome to BadgerWorks</p><br/> </h1>
-            <a href="/assets/Resume_Public.pdf" class="button button-inverse button-rounded"><i class="fa fa-download"></i> Resume</a>
-            <p class="lead">
-            
-              <div class="btn-group">
-                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Projects <span class="caret"></span>
-                </button>
-                <ul class="dropdown-menu">
-                  <li><a href="/crawler">Internet Crawler</li>
-                  <li role="separator" class="divider"></li>
-                  <li><a href="http://chat.badgerworks.org/">Room-based Websocket Chat</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li><a href="/game">2D Game</a></li>
-                </ul>
-                </p>
-          </div>
           <!-- <a href="/nyan"><h1>&#x1f431;</h1></a>-->
-          </p>
+          
           </div>
 
 
--- application/views/pages/projects.php
+++ application/views/pages/projects.php
@@ -1,23 +1,29 @@
 <div class="inner cover topmid page">
-        <div class="container">
       <!-- Example row of columns -->
       <div class="row">
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        <div class="col-md-4 project">
+          <h2>Multithreaded Web Crawler</h2>
+          <p>Maven project written in Java using Threads, the crawler crawls webpages and mines for links and queues them for simultaneous crawling. Data is stored in PostreSQL using JDBC.
+          <p><a class="btn btn-default" href="/crawler" role="button">View details &raquo;</a></p>
         </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        <div class="col-md-4 project">
+          <h2>Simple Bio Simulation</h2>
+          <p>Written in Java using Canvas. Simulation of Bacterial Cell, White Blood Cells, their movement and lifecycle. Project for the OOP course.</p>
+          <p><a class="btn btn-default" href="/biosim" role="button">View details &raquo;</a></p>
        </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        <div class="col-md-4 project">
+          <h2>Websocket group chat application with giphy bot</h2>
+          <p>Wrote the server and the http client for a roombased chat app using NodeJS. Made the messages jQuery injection safe. Added a giphy bot that fetches related gifs. Added Simple messages achieving</p> 
+          <p><a class="btn btn-default" href="http://chat.badgerworks.org" role="button">View Demo &raquo;</a></p>
         </div>
       </div>
-    	</div>
 
+     <div class="row">
+        <div class="col-md-4 project">
+          <h2>2D Game</h2>
+          <p>Written in C# using the Unity3D API. Random generation of bases for unending gameplay until gameover condition reached</p> 
+          <p><a class="btn btn-default" href="/game" role="button">View Demo &raquo;</a></p>
+        </div>
+      </div>
+   
 </div>
